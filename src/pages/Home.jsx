4ef79a2c966e7d5a4be963f8d851fce32083b655
Javier773 <javimoreno20@hotmail.com>
2023-06-16 14:06:00 +0200

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -60,7 +60,7 @@ const Home = () => {
   return (
     <div className='home'>
       <div className='posts'>
-        {posts.length === 0 ? "" : posts?.map((post) => (
+        {!posts ? "" : posts?.map((post) => (
           <div className="post" key={post?.id}>
             <div className="img">
               <img src={`../uploads/${post?.img}`} alt="" />
