038cd7c3291b09a28c36d6567af645d0ad32d6ea
Javier773 <javimoreno20@hotmail.com>
2023-06-16 14:10:30 +0200

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(`/posts${cat}`);
+        const res = await axios.get(`http://localhost:8800/api/posts`);
         setPosts(res.data);
       } catch (error) {
         console.log(error);
