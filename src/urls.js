95e9a1935050049db462c6e02f383275fbd9828f
Javier773 <javimoreno20@hotmail.com>
2023-06-17 16:29:23 +0200

    update

--- src/urls.js
+++ src/urls.js
@@ -1,3 +1,3 @@
 
-// export const url = "https://blog-api-six-silk.vercel.app/api/";
-export const url = "http://localhost:8800/api/"
+export const url = "https://blog-api-six-silk.vercel.app/api/";
+// export const url = "http://localhost:8800/api/"
